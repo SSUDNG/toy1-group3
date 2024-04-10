a0408568f10f07de7caf2c8d6e2d0b8916ae0716
lsh <jetaime12121@gmail.com>
2024-04-10 16:32:08 +0900

    login jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Outlet, Route, Routes } from "react-router-dom";
+import Nav from "components/Nav";
+import TestPage from "./pages/TestPage";
+import LoginPage from "./pages/LoginPage";
+
+const Layout = () => {
+  return (
+    <div>
+      <Nav />
+
+      <Outlet />
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<LoginPage />} />
+          <Route path="/main" element={<TestPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
