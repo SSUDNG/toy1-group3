ceaa2b26850a8c425ca8dcc6e679d10cf3bba99c
SangHwa <kimsh9026@gmail.com>
2024-04-11 12:14:11 +0900

    import React-Router-DOM, MUI

---
+++ src/pages/TAAListPage/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TextField from "@mui/material/TextField";
+
+function TAAListPage() {
+  return (
+    <TextField id="outlined-basic" label="TAAListPage" variant="outlined" />
+  );
+}
+
+export default TAAListPage;
