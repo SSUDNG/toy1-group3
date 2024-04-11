913e6eff6851129e39e9cbc76b5b7fec6f38c0de
SangHwa <kimsh9026@gmail.com>
2024-04-11 12:50:13 +0900

    add _redirects

--- src/pages/TAAListPage/index.tsx
+++ src/pages/TAAListPage/index.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import TextField from "@mui/material/TextField";
 
 function TAAListPage() {
-  return (
-    <TextField id="outlined-basic" label="TAAListPage" variant="outlined" />
-  );
+  return <TextField id="outlined-basic" label="Outlined" variant="outlined" />;
 }
 
 export default TAAListPage;
