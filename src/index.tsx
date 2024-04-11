5f5984f4f83dddaaf0f496fb1337e31ba9428df5
Nicola <159132327+nicola-laura@users.noreply.github.com>
2024-04-11 18:47:23 +0900

    edit: deleted styled components, applied MUI

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./App";
-import Request from "pages/Request";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
@@ -10,7 +10,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Request />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
 
--- src/pages/Request.tsx
+++ src/pages/Request.tsx
@@ -1,116 +1,21 @@
 import React, { useState } from "react";
-import styled from "styled-components";
-import "../global.css";
-
-const Container = styled.div`
-  padding: 30px;
-  margin: 20px;
-  border-radius: 10px;
-  background-color: white;
-`;
-
-const Title = styled.h1`
-  font-size: 26px;
-  margin-top: 0;
-  margin-bottom: 20px;
-`;
-
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-`;
-
-const DateContainer = styled.div`
-  display: flex;
-  gap: 10px;
-`;
-
-const Label = styled.label`
-  display: flex;
-  flex-direction: column;
-  gap: 5px;
-`;
-
-const LabelText = styled.p`
-  font-size: 16px;
-  margin-top: 0;
-  margin-bottom: 5px;
-`;
-
-const Select = styled.select`
-  padding: 8px;
-  border: 1px solid #dddddd;
-  border-radius: 6px;
-  color: #818181;
-
-  option#placeholder {
-    color: #818181;
-  }
-
-  option:checked {
-    color: #333;
-  }
-`;
-
-const Input = styled.input`
-  padding: 8px;
-  border: 1px solid #dddddd;
-  border-radius: 6px;
-  color: #818181;
-`;
-
-const NotesInput = styled(Input)`
-  height: 100px;
-  color: #333;
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  gap: 10px;
-`;
-
-const Button = styled.button`
-  max-width: 120px;
-  width: 100%;
-  padding: 10px;
-  background-color: #333;
-  color: #fff;
-  border-radius: 6px;
-  border: none;
-  font-size: 14px;
-`;
-
-const SubmitButton = styled(Button)`
-  background-color: #4724ac;
-`;
+import {
+  Button,
+  TextField,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Grid,
+} from "@mui/material";
+import "./Request.css";
 
 const Request: React.FunctionComponent = function Request() {
   const [vacationType, setVacationType] = useState("");
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
   const [reason, setReason] = useState("");
   const [notes, setNotes] = useState("");
-
-  const handleVacationTypeChange = (
-    event: React.ChangeEvent<HTMLSelectElement>,
-  ) => {
-    setVacationType(event.target.value);
-  };
-  const handleStartDateChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    setStartDate(event.target.value);
-  };
-  const handleEndDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEndDate(event.target.value);
-  };
-  const handleReasonChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setReason(event.target.value);
-  };
-  const handleNotesChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNotes(event.target.value);
-  };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -123,65 +28,94 @@ const Request: React.FunctionComponent = function Request() {
     });
   };
 
+  const handleReset = () => {
+    setVacationType("");
+    setStartDate(null);
+    setEndDate(null);
+    setReason("");
+    setNotes("");
+  };
+
   return (
-    <Container>
-      <Title>근태신청</Title>
-      <Form onSubmit={handleSubmit}>
-        <Label htmlFor={vacationType}>
-          <LabelText>휴가 종류</LabelText>
-          <Select value={vacationType} onChange={handleVacationTypeChange}>
-            <option value="" id="placeholder">
-              선택하세요
-            </option>
-            <option value="연차">연차</option>
-            <option value="반차">반차</option>
-            <option value="병가">병가</option>
-            <option value="조퇴">조퇴</option>
-            <option value="기타">기타</option>
-          </Select>
-        </Label>
-        <DateContainer>
-          <Label htmlFor={startDate}>
-            <LabelText>시작일</LabelText>
-            <Input
-              type="date"
-              value={startDate}
-              onChange={handleStartDateChange}
-            />
-          </Label>
-          <Label htmlFor={endDate}>
-            <LabelText>종료일</LabelText>
-            <Input type="date" value={endDate} onChange={handleEndDateChange} />
-          </Label>
-        </DateContainer>
-        <Label htmlFor={reason}>
-          <LabelText>사유</LabelText>
-          <Select value={reason} onChange={handleReasonChange}>
-            <option value="" id="placeholder">
-              선택하세요
-            </option>
-            <option value="휴가">휴가</option>
-            <option value="병원방문">병원방문</option>
-            <option value="개인사유">개인사유</option>
-            <option value="비상">비상</option>
-            <option value="기타">기타</option>
-          </Select>
-        </Label>
-        <Label htmlFor={notes}>
-          <LabelText>메모</LabelText>
-          <NotesInput
-            id="notesInput"
-            type="text"
-            value={notes}
-            onChange={handleNotesChange}
+    <form className="form-container" onSubmit={handleSubmit}>
+      <h1>부재 신청</h1>
+      <Grid className="grid-container" container spacing={2}>
+        <Grid item xs={12}>
+          <FormControl fullWidth>
+            <InputLabel>휴가 종류</InputLabel>
+            <Select
+              value={vacationType}
+              onChange={(e) => setVacationType(e.target.value as string)}
+            >
+              <MenuItem value="연차">연차</MenuItem>
+              <MenuItem value="반차">반차</MenuItem>
+              <MenuItem value="병가">병가</MenuItem>
+              <MenuItem value="조퇴">조퇴</MenuItem>
+              <MenuItem value="기타">기타</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            label="시작일"
+            type="date"
+            fullWidth
+            value={startDate?.toISOString().split("T")[0]}
+            onChange={(e) => setStartDate(new Date(e.target.value))}
+            InputLabelProps={{
+              shrink: true,
+            }}
           />
-        </Label>
-        <ButtonContainer>
-          <SubmitButton type="submit">신청하기</SubmitButton>
-          <Button type="button">취소하기</Button>
-        </ButtonContainer>
-      </Form>
-    </Container>
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            label="종료일"
+            type="date"
+            fullWidth
+            value={endDate?.toISOString().split("T")[0]}
+            onChange={(e) => setEndDate(new Date(e.target.value))}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <FormControl fullWidth>
+            <InputLabel>사유</InputLabel>
+            <Select
+              value={reason}
+              onChange={(e) => setReason(e.target.value as string)}
+            >
+              <MenuItem value="휴가">연차</MenuItem>
+              <MenuItem value="병원방문">반차</MenuItem>
+              <MenuItem value="개인사유">병가</MenuItem>
+              <MenuItem value="비상">조퇴</MenuItem>
+              <MenuItem value="기타">기타</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            label="추가 정보"
+            fullWidth
+            multiline
+            rows={4}
+            value={notes}
+            onChange={(e) => setNotes(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <Button variant="contained" color="primary" type="submit">
+            신청하기
+          </Button>
+        </Grid>
+        <Grid item xs={6}>
+          <Button variant="contained" color="secondary" onClick={handleReset}>
+            취소하기
+          </Button>
+        </Grid>
+      </Grid>
+    </form>
   );
 };
 
