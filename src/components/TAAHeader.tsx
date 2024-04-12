0a9012e7e1b5d0aeb2ef1495bb0d8b80496d4117
SangHwa <kimsh9026@gmail.com>
2024-04-12 15:02:05 +0900

    TAA filter

--- src/components/TAAHeader.tsx
+++ src/components/TAAHeader.tsx
@@ -1,12 +1,39 @@
-import { Container } from "@mui/material";
+import {
+  Container,
+  Button,
+  Select,
+  MenuItem,
+  SelectChangeEvent,
+} from "@mui/material";
 import React from "react";
+import styles from "./TAA.module.css";
 
-function TAAHeader() {
+interface TAAHeaderProps {
+  current: string;
+  onSelect: (type: string) => void;
+}
+
+function TAAHeader({ current, onSelect }: TAAHeaderProps) {
+  const selectType = (e: SelectChangeEvent) => {
+    onSelect(e.target.value);
+  };
+
   return (
-    <Container>
-      <h1>근태신청</h1>
-      <span>근태신청해주세요</span>
-      <button type="button">근태신청</button>
+    <Container className={styles.headerContainer}>
+      <h1>근태신청 목록</h1>
+
+      <Select value={current} onChange={selectType}>
+        <MenuItem value="전체">전체</MenuItem>
+        <MenuItem value="연차">연차</MenuItem>
+        <MenuItem value="반차">반차</MenuItem>
+        <MenuItem value="병가">병가</MenuItem>
+        <MenuItem value="조퇴">조퇴</MenuItem>
+        <MenuItem value="기타">기타</MenuItem>
+      </Select>
+
+      <Button variant="contained" size="large" className={styles.btn}>
+        근태신청하기
+      </Button>
     </Container>
   );
 }
--- src/components/TAAList.tsx
+++ src/components/TAAList.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Container from "@mui/material/Container";
+import { Container } from "@mui/material";
+import styles from "./TAA.module.css";
 import { AttandanceInfo } from "./TypeDef";
 
 type TAAListProps = {
@@ -9,20 +10,21 @@ type TAAListProps = {
 export default function TAAList({ TAAdata }: TAAListProps) {
   return (
     <Container fixed>
-      <div>
-        {TAAdata.map((obj: AttandanceInfo) => (
-          <div key={obj.key}>
-            <span>{obj.name}</span>
-            <span>{obj.category}</span>
-            <span>
+      {TAAdata.map((obj: AttandanceInfo) => (
+        <Container key={obj.key} className={styles.itemContainer}>
+          <Container className={styles.coreInfo}>
+            <div>{obj.name}</div>
+            <div>{obj.category}</div>
+            <div>
               {obj.begin.year}-{obj.begin.month}-{obj.begin.day}
-            </span>
-            <span>
+            </div>
+            <div>
               {obj.end.year}-{obj.end.month}-{obj.end.day}
-            </span>
-          </div>
-        ))}
-      </div>
+            </div>
+          </Container>
+          <Container className={styles.contents}>{obj.comment}</Container>
+        </Container>
+      ))}
     </Container>
   );
 }
