6c47fdd60f2dd479040ed41a70103ebd6fab6859
Nicola <159132327+nicola-laura@users.noreply.github.com>
2024-04-16 15:03:59 +0900

    add: clock component

---
+++ src/components/Clock.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import Typography from "@mui/material/Typography";
+
+const Clock: React.FunctionComponent = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    weekday: "long",
+  };
+
+  const dateString = time.toLocaleDateString("ko-KR", options);
+  const timeString = time.toLocaleTimeString("ko-KR");
+
+  return (
+    <div>
+      <Typography variant="h1">{timeString}</Typography>
+      <Typography variant="body2">{dateString}</Typography>
+    </div>
+  );
+};
+
+export default Clock;
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
+import Clock from "components/Clock";
+import styles from "components/Clock.module.css";
 
 function MainPage() {
   const navigate = useNavigate();
@@ -9,13 +11,18 @@ function MainPage() {
   };
 
   return (
-    <Button
-      onClick={() => {
-        goToTAAList();
-      }}
-    >
-      근태신청목록
-    </Button>
+    <div>
+      <Button
+        onClick={() => {
+          goToTAAList();
+        }}
+      >
+        근태신청목록
+      </Button>
+      <div className={styles.clock}>
+        <Clock />
+      </div>
+    </div>
   );
 }
 
